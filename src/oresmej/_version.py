e4815a3345196bc4540ad98c3c50d6891f0ae13b
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-07 13:45:20 +0300

    Update license and dependencies in _version.py

--- src/oresmej/_version.py
+++ src/oresmej/_version.py
@@ -1,9 +1,9 @@
 # _version.py
 
 __version__ = "0.1.3"
-__license__ = "MIT"
+__license__ = "GPL-3.0-or-later"
 __description__ = "Oresme numbers refer to the sums related to the harmonic series."
 __author__ = "Mehmet Keçeci"
 __url__ = "https://github.com/WhiteSymmetry/oresmej"
 __docs__ = "https://WhiteSymmetry.github.io/oresmej"  # Opsiyonel: Dokümantasyon linki
-__dependencies__ = ["python>=3.9"]  # Diğer bağımlılıkları da ekleyebilirsiniz
+__dependencies__ = ["python>=3.11"]  # Diğer bağımlılıkları da ekleyebilirsiniz
