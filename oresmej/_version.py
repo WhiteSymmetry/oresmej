2840f915b5eb75516d01f6adc87405176855f8f1
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-11 12:36:46 +0300

    Update _version.py

--- oresmej/_version.py
+++ oresmej/_version.py
@@ -1,6 +1,6 @@
 # _version.py
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __license__ = "AGPL-3.0-or-later"
 __description__ = "Oresme numbers refer to the sums related to the harmonic series."
 __author__ = "Mehmet Keçeci"
